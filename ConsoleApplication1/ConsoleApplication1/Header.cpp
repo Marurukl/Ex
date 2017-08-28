ef2d8e4bae322d67bcb3e3b7ca32fef4a0216ff2
lRaika <arnurelnur@gmail.com>
2017-08-28 18:22:32 +0600

    ar

--- ConsoleApplication1/ConsoleApplication1/Header.cpp
+++ ConsoleApplication1/ConsoleApplication1/Header.cpp
@@ -19,83 +19,131 @@ namespace calculator
 			if (kbhit()) {
 				key = getch();
 				std::cout << key;
-				if (key == 0) {
+
+				if (key == 48) {
 					break;
 				}
-				else if (key == 1) {
+				else if (key == 27) {
+					num1 = num2 = 0;
+					result = 0;
+					ShowMenu();
+				}
+				else if (key == 49) {
+					
+					ShowMenu();
+				}
+				else if (key == 50) {
+					ChangeSignNumber();
+					ShowMenu();
+				}
+				else if (key == 51) {
+					AdditionNumber();
+					ShowMenu();
+				}
+				else if (key == 52) {
+					SubtractionNumber();
+					ShowMenu();
+				}
+				else if (key == 53) {
+					DivisionNumber();
+					ShowMenu();
+				}
+				else if (key == 54) {
+					MultiplicationNumber();
+					ShowMenu();
+				}
+				else if (key == 55) {
+					TakingFromSquare();
+					ShowMenu();
 
 				}
-				else if (key == 2) {
+				else if (key == 56) {
+					ErectioToTheSquare();
+					ShowMenu();
 
 				}
-				else if (key == 3) {
-
-				}
-				else if (key == 4) {
-
-				}
-				else if (key == 5) {
-
-				}
-				else if (key == 6) {
-
-				}
-				else if (key == 7) {
-
-				}
-				else if (key == 8) {
-
-				}
-				else if (key == 9) {
+				else if (key == 57) {
+					OneDivideX();
+					ShowMenu();
 
 				}
 			}
-			ShowMenu();
 		}
 		
 	}
 	void Calculator::ChangeSignNumber() {
-		~num1 + 1;
+		SetNumber1();
+		num1 = -num1;
+		std::cout << "\n" << num1;
+		std::cin.get(); std::cin.get();
 	}
 	void Calculator::SetNumber1() {
-		std::cout << "Введите 1 цифру : ";
+		std::cout << "\nВведите 1 цифру : ";
 		std::cin >> num1;
 
 	}
 	void Calculator::SetNumber2() {
-		std::cout << "Введите 2 цифру : ";
+		std::cout << "\nВведите 2 цифру : ";
 		std::cin >> num2;
 	}
 	void Calculator::AdditionNumber()
 	{
+		SetNumber1();
+		SetNumber2();
 		result = num1 + num2;
+		std::cout << "\n" << result;
+		std::cin.get(); std::cin.get();
 	}
 	void Calculator::SubtractionNumber()
 	{
+		SetNumber1();
+		SetNumber2();
 		result = num1 - num2;
+		std::cout << "\n" << result;
+		std::cin.get(); std::cin.get();
 	}
 	void Calculator::DivisionNumber() 
 	{
+		SetNumber1();
+		SetNumber2();
 		result = num1 / num2;
+		std::cout << "\n" << result;
+		std::cin.get(); std::cin.get();
 	}//Деление
 	void Calculator::MultiplicationNumber()
 	{
+		SetNumber1();
+		SetNumber2();
 		result = num1 * num2;
+		std::cout << "\n" << result;
+		std::cin.get(); std::cin.get();
 	}//Умножение
 	void Calculator::TakingFromSquare() 
 	{
+		SetNumber1();
 		result = sqrt((double)num1);
+		std::cout << "\n" << result;
+		std::cin.get(); std::cin.get();
 	}//Взятие из под квадратного корня
 	void Calculator::ErectioToTheSquare() 
 	{
+		SetNumber1();
 		result = num1 * num1;
+		std::cout << "\n" << result;
+		std::cin.get(); std::cin.get();
 	}//Возведение к квадрат
 	void Calculator::OneDivideX()
 	{
-		result = 1 / num1;
+		SetNumber1();
+		double number;
+		number = num1;
+		result = 1 / number;
+		std::cout << "\n" << result;
+		std::cin.get(); std::cin.get();
 	}//1/х
 	void Calculator::ShowMenu() {
 		system("cls");
-		std::cout << " 0 - выход /n 1 -	Написать цифру /n  2 - Смена знака у числа на противоположный /n 3 - Сложение /n 4 - Вычитание /n 5 - Деление /n 6 - Умножение /n 7 - Взятие из под квадратного корня /n 8 - Возведение к квадрат /n 9 - 1/х";
+		std::cout << " 0 - выход \n 1 -	Написать цифру \n  2 - Смена знака у числа на противоположный \n 3 - Сложение \n 4 - Вычитание \n 5 - Деление \n 6 - Умножение \n 7 - Взятие из под квадратного корня \n 8 - Возведение к квадрат \n 9 - 1/х";
 	}
+	
 }
